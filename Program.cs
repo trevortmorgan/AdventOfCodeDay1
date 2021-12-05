1b6a35b52aeffe349f26d496ec8f530ced72f2c8
Trevor Morgan <trevortmorgan@gmail.com>
2021-12-05 13:59:10 +0000

    Initial Commit - Reading in file from Local Directory

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("What is your name?");
+            // var name = Console.ReadLine();
+            // var currentDate = DateTime.Now;
+            // Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            // Console.Write($"{Environment.NewLine}Press any key to exit...");
+            // Console.ReadKey(true);
+            string path = "static/sonarReadings.txt";
+            if(File.Exists(path)) {
+                Console.WriteLine("exists!");
+            } else {
+                Console.WriteLine("Doesn't Exist");  
+            }
+
+            Console.WriteLine("test");
+
+        }
+    }
+}
